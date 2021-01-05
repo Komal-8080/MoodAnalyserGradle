620e976130b38a5d0cd3886e600c456c686f4138
Komal-8080 <shindeg163@gmail.com>
2021-01-05 13:27:54 +0530

    Added: method to analyse and respond Happy or sad mood added

--- main/java/com/MoodAnalysisTestGradle/MoodAnalyser.java
+++ main/java/com/MoodAnalysisTestGradle/MoodAnalyser.java
@@ -1,6 +1,11 @@
 package com.MoodAnalysisTestGradle;
 
 public class MoodAnalyser {
-
-	
+	public String analyseMood(String message) {
+		if(message.contains(("sad"))) {
+			return "SAD";			
+		} else {
+			return "Happy";
+		}	
+	}	
 }
--- test/java/com/MoodAnalysisTestGradle/MoodAnalyserTest.java
+++ test/java/com/MoodAnalysisTestGradle/MoodAnalyserTest.java
@@ -1,8 +1,18 @@
 package com.MoodAnalysisTestGradle;
 
-
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MoodAnalyserTest {
 	
+	public void testMoodAnalysis() {
+		MoodAnalyser moodAnlayser = new MoodAnalyser();
+		String mood = moodAnlayser.analyseMood("Iam in sad mood");
+		
+		//Assert Statement
+		Assert.assertThat(mood, CoreMatchers.is("SAD"));
+		
+	}
 	
 }
